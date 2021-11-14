9fdc6e7922a8f449e9ffeacc0c1b0e1349235d23
Anshika Jain <jainanshika193@gmail.com>
2021-11-14 17:31:02 +0530

    changes

--- frontend/src/Pages/Test/Test.js
+++ frontend/src/Pages/Test/Test.js
@@ -4,7 +4,13 @@ import { useParams } from "react-router-dom";
 function Test() {
   let { testID } = useParams();
 
-  return <div>{testID}</div>;
+  return(
+    <div>
+      <h1>OOSE CLASS TEST-1</h1>
+      <p>15 MCQs, 20 min</p>
+       
+    </div>
+  );
 }
 
 export default Test;
